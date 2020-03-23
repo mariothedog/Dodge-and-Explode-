9be095ff37d1131c373707eb94668d48e0852d09
mariothedog <mariothedogmc@gmail.com>
2020-03-23 19:12:34 +0000

    Added slow motion when an enemy is in range.

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,6 +6,19 @@ const SPEED = 300
 # Movement variables
 var velocity = Vector2()
 
+# Slow-mo constants
+const END_SPEED = 1
+
+# Slow-mo variables
+var slow_mo_enabled = false
+var slow_mo_fade_out = false
+var time_start
+var fade_duration_ms
+var start_speed
+
+func _ready():
+	Engine.time_scale = 1
+
 func _physics_process(_delta):
 	get_input()
 	movement()
@@ -30,3 +43,34 @@ func movement():
 func die():
 	if get_tree().reload_current_scene() != OK:
 		print_debug("An error occured while reloading the current scene.")
+
+func _on_Slowmo_Activator_body_entered(_body):
+	start_slow_mo()
+
+func _on_Slowmo_Activator_body_exited(_body):
+	stop_slow_mo()
+
+func _process(_delta):
+	if slow_mo_fade_out:
+		var current_time = OS.get_ticks_msec() - time_start
+		var time_scale = circ_ease_in(current_time, start_speed, END_SPEED, fade_duration_ms)
+		if current_time >= fade_duration_ms:
+			slow_mo_fade_out = false
+			slow_mo_enabled = false
+			time_scale = END_SPEED
+		Engine.time_scale = time_scale
+
+func start_slow_mo(start_strength = 0.9):
+	time_start = OS.get_ticks_msec()
+	start_speed = 1 - start_strength
+	Engine.time_scale = start_speed
+	slow_mo_enabled = true
+
+func stop_slow_mo(fade_duration = 0.2):
+	fade_duration_ms = fade_duration * 1000
+	slow_mo_fade_out = true
+
+func circ_ease_in(t, b, c, d):
+	# http://gizma.com/easing/
+	t /= d
+	return -c * (sqrt(1 - t * t) - 1 ) + b
