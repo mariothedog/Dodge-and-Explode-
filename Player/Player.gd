4a2e96042c7bd07dc807eb5e673adc3919113a56
mariothedog <mariothedogmc@gmail.com>
2020-03-23 15:08:30 +0000

    Added a basic player.

---
+++ Player/Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+# Movement constants
+const SPEED = 300
+
+# Movement variables
+var velocity = Vector2()
+
+func _physics_process(delta):
+	get_input()
+	movement()
+
+func get_input():
+	var input_vel = Vector2()
+	
+	if Input.is_action_pressed("move_right"):
+		input_vel.x += 1
+	if Input.is_action_pressed("move_left"):
+		input_vel.x -= 1
+	if Input.is_action_pressed("move_up"):
+		input_vel.y -= 1
+	if Input.is_action_pressed("move_down"):
+		input_vel.y += 1
+	
+	velocity = input_vel.normalized() * SPEED
+
+func movement():
+	velocity = move_and_slide(velocity)
