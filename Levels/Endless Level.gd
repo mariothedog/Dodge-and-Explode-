a23f0b2cc7189277fd9b48d6c950e62aead0b05e
mariothedog <mariothedogmc@gmail.com>
2020-03-25 14:49:52 +0000

    Added a slight screen bend and renamed some things.

--- Levels/Endless Level.gd
+++ Levels/Endless Level.gd
@@ -5,7 +5,7 @@ var enemy_scene = preload("res://Enemies/Enemy.tscn")
 func _ready():
 	randomize()
 	
-	$"Slow-mo Visual Effect".material.set_shader_param("apply", false)
+	$"Visual Effect".material.set_shader_param("slow_mo", false)
 
 func _on_Spawn_Enemy_timeout():
 	var enemy = enemy_scene.instance()
@@ -40,21 +40,21 @@ func get_random_position_outside_level():
 	return pos
 
 func _on_Player_slow_mo_enabled():
-	$"Slow-mo Visual Effect".material.set_shader_param("apply", true)
+	$"Visual Effect".material.set_shader_param("slow_mo", true)
 
 func _on_Player_slow_mo_disabled():
-	$"Slow-mo Visual Effect".material.set_shader_param("apply", false)
+	$"Visual Effect".material.set_shader_param("slow_mo", false)
 
 func _process(_delta):
 	if $Player.slow_mo_enabled:
 		# When the time scale is 0.1 the blur_amount/chromatic_amount will be 0.5.
 		# When the time_scale is 1 the blur_amount/chromatic_amount will be 0.
 		var amount = -5.0/9.0 * Engine.time_scale + 5.0/9.0
-		$"Slow-mo Visual Effect".material.set_shader_param("blur_amount", amount)
+		$"Visual Effect".material.set_shader_param("blur_amount", amount)
 		
-		$"Slow-mo Visual Effect".material.set_shader_param("chromatic_amount", amount)
+		$"Visual Effect".material.set_shader_param("chromatic_amount", amount)
 		
 		# When the time_scale is 1 the darkness_multi will also be 1.
 		# When the time_scale is 0.1 the darkness_multi will be 0.8.
 		var darkness_multi = 2.0/9.0 * Engine.time_scale + 7.0/9.0
-		$"Slow-mo Visual Effect".material.set_shader_param("darkness_multi", darkness_multi)
+		$"Visual Effect".material.set_shader_param("darkness_multi", darkness_multi)
