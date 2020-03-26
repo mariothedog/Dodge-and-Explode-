e6a119aecce80c230c7b3080d5659aeea3ab8be4
mariothedog <mariothedogmc@gmail.com>
2020-03-26 20:07:27 +0000

    Added an enemy death animation.

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -8,14 +8,14 @@ var target_dir
 
 func _ready():
 	$CollisionShape2D.set_deferred("disabled", true)
+	
+	if target_dir:
+		velocity = target_dir * SPEED
 
 func _physics_process(_delta):
 	movement()
 
 func movement():
-	if target_dir:
-		velocity = target_dir * SPEED
-	
 	velocity = move_and_slide(velocity)
 	
 	for i in range(get_slide_count()):
@@ -33,4 +33,24 @@ func _on_Tile_Collision_Enabler_body_exited(_body):
 	$CollisionShape2D.set_deferred("disabled", false)
 
 func die():
-	queue_free()
+	velocity = Vector2.ZERO
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+	play_death_animation()
+	
+	yield($Tween, "tween_all_completed")
+	var timer = get_tree().create_timer(0.1)
+	timer.connect("timeout", self, "queue_free")
+
+func play_death_animation():
+	for segment in $Body.get_children():
+		var segment_dir = (segment.rect_position + segment.rect_pivot_offset).normalized()
+		
+		segment_dir = segment_dir.rotated(deg2rad(rand_range(0, 45)))
+			
+		var final_pos = segment.rect_position + segment_dir * 40
+		$Tween.interpolate_property(segment, "rect_position", segment.rect_position, final_pos, 0.1, Tween.TRANS_QUAD, Tween.EASE_OUT)
+		$Tween.start()
+		
+		$Tween.interpolate_property(segment, "color", Color(1, 0, 0, 1), Color(1, 0, 0, 0), 0.5)
+		$Tween.start()
