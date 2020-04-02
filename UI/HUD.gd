cd1468c0eb0f3afbec96ae05af75d2ff9f3a7524
mariothedog <mariothedogmc@gmail.com>
2020-04-03 00:33:18 +0100

    Changed the restart animation's easing for the score as on the web version it goes into the negatives.

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -12,7 +12,7 @@ func _on_Enemy_dead(_cause):
 	update_score(score)
 
 func _on_Endless_Level_manually_restarting():
-	$Tween.interpolate_property(self, "score", score, 0, 0.5, Tween.TRANS_CIRC, Tween.EASE_IN_OUT)
+	$Tween.interpolate_property(self, "score", score, 0, 0.5)
 	$Tween.start()
 
 func _on_Tween_tween_step(_object, _key, _elapsed, _value):
