81256cb2c04f439c6239ade3e99856dbb6b0ddd8
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-06-28 22:20:58 +0100

    Initial Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Dodge-and-Explode-
-Made for Major Jam which had the theme: "Isolation" and the limitation "No living things".
--- global.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var screen_shake_enabled = true
-var audio_enabled = true
-var music_volume = 0
-var audio_volume = 0
-
-func rand_int(minimum, maximum): # Does not include the maximum value.
-	return randi() % maximum + minimum
