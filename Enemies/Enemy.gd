6a1f2513c41bcd4e9c5b5cc30d227f54fe4f5469
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-06-29 14:01:34 +0100

    Enemies are now only visible when in the play area.

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -1,14 +1,28 @@
 extends Area2D
 
+# Constants
 const SPEED = 300
 
+# Variables
 var target_dir = Vector2()
-
 var velocity = Vector2()
+
+func _ready():
+	visible = false
 
 func _physics_process(delta):
 	velocity = target_dir * SPEED
 	position += velocity * delta
 
+func die():
+	queue_free()
+
 func _on_Enemy_body_entered(body):
-	body.die()
+	if body.name == "Player":
+		body.die()
+	elif body.name == "Walls" and visible:
+		die()
+
+func _on_Enemy_body_exited(body):
+	if body.name == "Walls":
+		visible = true
