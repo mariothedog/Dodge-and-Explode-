abbd3f458e3639b8cde850ce1425956d5fff3e88
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-07-01 11:49:18 +0100

    Changed the player sprite and level palette.

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,31 +10,45 @@ const SPEED = 400
 
 # Variables
 var velocity = Vector2()
+var input_vel = Vector2()
 var dead = false
 
 func _unhandled_input(_event):
 	if dead:
 		return
 	
-	var input_vel = Vector2()
+	input_vel = Vector2()
 	input_vel.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	input_vel.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	velocity = input_vel.normalized() * SPEED
 
 func _physics_process(_delta):
+	move()
+	animate()
+
+func move():
 	velocity = move_and_slide(velocity)
+
+func animate():
+	if input_vel != Vector2.ZERO:
+		sprite.rotation = input_vel.angle() + PI/2
+	
+	if dead:
+		return
+	
+	if abs(velocity.length()) > 1:
+		animation_player.play("Run") # TODO
+	else:
+		animation_player.play("Idle") # TODO
 
 func die():
 	collision_shape.set_deferred("disabled", true)
 	dead = true
 	velocity = Vector2.ZERO
 	
-	animation_player.play("Explode")
+	animation_player.play("Die") # TODO
 	
 	yield(animation_player, "animation_finished")
-	
-	sprite.material.set_shader_param("time", 0)
-	sprite.material.set_shader_param("segment_color", Color("0d0c0c"))
 	
 	if get_tree().reload_current_scene() != OK:
 		print_debug("An error occurred while reloading the scene.")
